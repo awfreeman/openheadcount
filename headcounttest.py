c612495e38912b134ff7c861783ca1d9a335b0ff
bioniclekinz <awfreeman1@gmail.com>
2021-10-18 20:27:03 -0400

    add mobilenetssd

--- headcounttest.py
+++
@@ -1,9 +0,0 @@
-from imutils.video import VideoStream
-from imutils.video import FPS
-import argparse
-import imutils
-import time
-import cv2
-from datetime import datetime, time
-import numpy as np
-import time as time2

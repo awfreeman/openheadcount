3aa2dded47c5c5491da44b7b836359541d844fa2
bioniclekinz <awfreeman1@gmail.com>
2021-10-20 17:16:43 -0400

    image resizing change

--- headcounttest.py
+++ headcounttest.py
@@ -4,4 +4,16 @@ import imutils
 
 model='mobnet/MobileNetSSD_deploy.caffemodel'
 prototxt='mobnet/MobileNetSSD_deploy.prototxt'
+#load in network
 net=cv.dnn.readNetFromCaffe(prototxt, model)
+#start video capture
+vc = cv.VideoCapture("rtsp://admin:@192.168.1.186:554/h264Preview_01_main")
+while vc.isOpened()==False:
+    continue
+while True:
+    rval, frame = vc.read()
+    frame = imutils.resize(frame, width=640, height=480)
+    rgb=cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    blob=cv.dnn.blobFromImage(frame)
+cv2.destroyWindow("preview")
+vc.release()

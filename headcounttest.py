987de0aa12d2ad8d3962aba4c38598963ee66fc2
awfreeman <awfreeman1@gmail.com>
2021-11-09 15:37:02 -0500

    add frameskip

--- headcounttest.py
+++ headcounttest.py
@@ -4,8 +4,8 @@ import imutils
 import time
 import numpy as np
 
-confidence=0.4
-
+confidence=0.2
+frameskip=5
 #load in network
 model='mobnet/MobileNetSSD_deploy.caffemodel'
 prototxt='mobnet/MobileNetSSD_deploy.prototxt'
@@ -20,27 +20,28 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 vc = cv.VideoCapture("rtsp://admin:@192.168.1.224:554/h264Preview_01_main")
 while vc.isOpened()==False:
     continue
+skip=0
 while True:
     rval, frame = vc.read()
-    frame = imutils.resize(frame, width=320, height=240)
-    rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    blob = cv.dnn.blobFromImage(image=frame, scalefactor=0.5, size=(320,240))
-    net.setInput(blob)
-    outputs = net.forward()
+    frame = imutils.resize(frame, width=1024, height=768)
     cv.imshow("preview", frame)
     key = cv.waitKey(20)
     if key == 27: # exit on ESC
         break
-    presence=False
-    for i in np.arange(0, outputs.shape[2]):
-        idx = int(outputs[0, 0, i, 1])
-        if CLASSES[idx] == "person" and float(outputs[0, 0 , i , 2])>confidence:
-            print(float(outputs[0, 0 , i , 2]))
-            presence = True
-    if presence:
-        print("there a dude in der")
-    else:
-        print("nobody here")
+    if skip == 5:
+        skip = 0
+        presence = 0
+        blob = cv.dnn.blobFromImage(image=frame, scalefactor=0.5, size=(1024,768))
+        net.setInput(blob)
+        outputs = net.forward()
+        for i in np.arange(0, outputs.shape[2]):
+            idx = int(outputs[0, 0, i, 1])
+            if CLASSES[idx] == "person" and float(outputs[0, 0 , i , 2])>confidence:
+                print(float(outputs[0, 0 , i , 2]))
+                presence=presence+1
+            
+        print(presence)
+    skip=skip+1
         
 		
 

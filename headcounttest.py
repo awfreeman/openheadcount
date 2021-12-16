7dd092d2a430ddd9d6c6ac656f4f5d88eab89276
awfreeman <awfreeman1@gmail.com>
2021-12-15 19:05:58 -0500

    fixed network stuff

--- headcounttest.py
+++ headcounttest.py
@@ -17,30 +17,31 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 		"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 		"sofa", "train", "tvmonitor"]
 #start video capture
-vc = cv.VideoCapture("rtsp://admin:@192.168.1.224:554/h264Preview_01_main")
+vc = cv.VideoCapture("yt1s.com - HD CCTV Camera video 3MP 4MP iProx CCTV HDCCTVCamerasnet retail store.mp4")
 while vc.isOpened()==False:
     continue
 skip=0
 while True:
     rval, frame = vc.read()
-    frame = imutils.resize(frame, width=1024, height=768)
+    frame = imutils.resize(frame, width=500)
     cv.imshow("preview", frame)
+    (H, W) = frame.shape[:2]
     key = cv.waitKey(20)
     if key == 27: # exit on ESC
         break
     if skip == frameskip:
         skip = 0
-        presence = 0
-        blob = cv.dnn.blobFromImage(image=frame, scalefactor=0.5, size=(1024,768))
+        #0.007843:normalizes values to between 0 and 2, 127.5: sets mean value to half brightness
+        blob = cv.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
         net.setInput(blob)
         outputs = net.forward()
         for i in np.arange(0, outputs.shape[2]):
             idx = int(outputs[0, 0, i, 1])
             if CLASSES[idx] == "person" and float(outputs[0, 0 , i , 2])>confidence:
-                print(float(outputs[0, 0 , i , 2]))
-                presence=presence+1
+                box = outputs[0, 0, i, 3:7] * np.array([W, H, W, H])
+                (startX, startY, endX, endY) = box.astype("int")
+                print(box)
             
-        print(presence)
     skip=skip+1
         
 		

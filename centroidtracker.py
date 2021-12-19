304d6de1e1256eb7263f9a67a19a6f50e68635f0
awfreeman <awfreeman1@gmail.com>
2021-12-19 14:25:39 -0500

    bullshit

---
+++ centroidtracker.py
@@ -0,0 +1,12 @@
+class Tracker:
+    objects: list = list()
+    threshold: float = 0
+    def __init__(self, newobjects: list, threshold: float):
+        self.objects = newobjects.copy()
+        self.threshold = threshold
+    def register():
+        pass
+    def deregister():
+        pass
+    def getClosest():
+        pass
--- headcounttest.py
+++ headcounttest.py
@@ -3,26 +3,27 @@ import argparse
 import imutils
 import time
 import numpy as np
+import centroidtracker as tracker
 
-confidence=0.1
-frameskip=10
+CONFIDENCE=0
 #load in network
-model='mobnet/MobileNetSSD_deploy.caffemodel'
-prototxt='mobnet/MobileNetSSD_deploy.prototxt'
-net=cv.dnn.readNetFromCaffe(prototxt, model)
-
+MODEL='mobnet/MobileNetSSD_deploy.caffemodel'
+PROTOTXT='mobnet/MobileNetSSD_deploy.prototxt'
 #list of classes that can be detected by model
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 		"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 		"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 		"sofa", "train", "tvmonitor"]
 #start video capture
-vc = cv.VideoCapture("yt1s.com - HD CCTV Camera video 3MP 4MP iProx CCTV HDCCTVCamerasnet retail store.mp4")
+vc = cv.VideoCapture("vid.mp4")
 while vc.isOpened()==False:
     continue
+#initialize network
+net=cv.dnn.readNetFromCaffe(prototxt, model)
+
+
 
 objects = list()
-skip=0
 while True:
     rval, frame = vc.read()
     frame = imutils.resize(frame, width=500)
@@ -30,26 +31,25 @@ while True:
     key = cv.waitKey(20)
     if key == 27: # exit on ESC
         break
-    if skip == frameskip:
-        skip = 0
-        #0.007843:normalizes values to between 0 and 2, 127.5: sets mean value to half brightness
-        blob = cv.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
-        net.setInput(blob)
-        outputs = net.forward()
+    #0.007843:normalizes values to between 0 and 2, 127.5: sets mean value to half brightness
+    blob = cv.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+    net.setInput(blob)
+    outputs = net.forward()
+    
+    newobjects = list()
+    for i in np.arange(0, outputs.shape[2]):
+        idx = int(outputs[0, 0, i, 1])
+        if CLASSES[idx] == "person" and float(outputs[0, 0 , i , 2])>CONFIDENCE:
+            #get new centroids
+            box = outputs[0, 0, i, 3:7] * np.array([W, H, W, H])
+            (startX, startY, endX, endY) = box.astype("int")
+            #add new centroids to list
+            newobjects.append(((startX+endX)//2,(startY+endY)//2))
+            
+            #frame = cv.rectangle(frame, pt1=(startX, startY), pt2=(endX,endY), color=(0,0,255), thickness=3)
+            #frame = cv.circle(frame, newobjects[-1], radius=3, color=(0,255,0), thickness=-1)
 
-        newobjects = list()
-        for i in np.arange(0, outputs.shape[2]):
-            idx = int(outputs[0, 0, i, 1])
-            if CLASSES[idx] == "person" and float(outputs[0, 0 , i , 2])>confidence:
-                box = outputs[0, 0, i, 3:7] * np.array([W, H, W, H])
-                (startX, startY, endX, endY) = box.astype("int")
-                newobjects.append(((startX+endX)//2,(startY+endY)//2))
-                frame = cv.rectangle(frame, pt1=(startX, startY), pt2=(endX,endY), color=(0,0,255), thickness=3)
-                frame = cv.circle(frame, newobjects[-1], radius=3, color=(0,255,0), thickness=-1)
-        cv.imshow("funny", frame)
-        
-    skip=skip+1
-        
+    cv.imshow("funny", frame)
 		
 
 cv.destroyWindow("preview")

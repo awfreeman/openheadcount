885380e05561b28674f45d0ed50eae9cf6a54b37
awfreeman <awfreeman1@gmail.com>
2022-01-18 17:49:05 -0500

    user authentication

--- usermgmt.py
+++ usermgmt.py
@@ -1,11 +1,32 @@
 import bcrypt
+import pandas
+import base64
 class usermgmt:
     def authenticate(uname:str, passwd:str):
-        users = open('conf/users', mode='r')
-        users.close()
-    def adduser(uname:str, passwd:str, permissions:str):
-        users = open('conf/users', mode='r+')
-        users.close()
+        logindf = pandas.read_csv('conf/login.csv')
+        logindf = logindf.query(f'username == @uname')
+        if len(logindf.index)==0:
+            return "No such user"
+        salt=logindf.iloc[0].salt
+        pwhash = str(bcrypt.hashpw(bytes(passwd, 'utf-8'), bytes(salt, 'utf-8')), 'utf-8')
+        spwhash = logindf.iloc[0].hash
+        if pwhash == spwhash:
+            return "successful login"
+        else:
+            return "lol dumbass"
+    def adduser(uname:str, passwd:str):
+        if len(passwd)>72:
+            return False, "Passwords must be under 72 characters long"
+        if ',' in uname:
+            return False, "No commas allowed in username"
+        logindf = pandas.read_csv('conf/login.csv')
+        if logindf.query(f'username == @uname').empty == False:
+            return "user already exists"
+        salt = bcrypt.gensalt()
+        pwhash = bcrypt.hashpw(bytes(passwd, 'utf-8'), salt)
+        pwhash = str(pwhash, 'utf-8')
+        salt = str(salt, 'utf-8')
+        logindf.loc[len(logindf.index)] = [uname, pwhash, salt]
+        logindf.to_csv('conf/login.csv', index=False)
     def rmuser(uname:str, passwd:str):
-        users = open('conf/users', mode='r+')
-        users.close()
+        pass
